8b2449332ef0cb7e0465999120ce23c9dde2bf6f
CvlNom <47310891+CvlNom@users.noreply.github.com>
2024-07-17 21:17:15 +0900

    Update Sequence.js

--- app/pcd/byElongation/Sequence.js
+++ app/pcd/byElongation/Sequence.js
@@ -118,7 +118,7 @@ const Sequence = () => {
 					<p>#12</p>
 				</div>
 				<div className='columns'>
-					<input type='text' value='Elongation(%)' readOnly />
+					<input type='text' value='E-Rate(%)' readOnly />
 					<input type='number' step='0.1' name='block1' value={inputRate.block1} onChange={handleChange0} />
 					<input type='number' step='0.1' name='block2' value={inputRate.block2} onChange={handleChange0} />
 					<input type='number' step='0.1' name='block3' value={inputRate.block3} onChange={handleChange0} />
